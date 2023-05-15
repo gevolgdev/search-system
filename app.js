5b237eebe92878506cc0417a2601c97d2a12b5e4
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-15 17:49:28 -0300

    recurso de pesquisa curta

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const inputSearch = document.querySelector('.input-text');
 const techContainer = document.querySelector('.tech-list');
+// ---------------------------------------------------------
 
 let search = '';
 
-const techs = ['Redux', 'React', 'Vue.js', 'Angular', 'Next.js', 'Tailwind CSS', 'GraphQL', 'Webpack', 'Storybook', 'Jest', 'ESLint', 'Mocha', 'Espruino'];
+const techs = [
+  'Redux', 'React', 'React Native', 'Vue.js', 'Angular', 'Next.js', 'Tailwind CSS', 'GraphQL', 'Webpack', 'Storybook', 'Jest', 'ESLint', 'Mocha', 'Espruino'
+].map(item => item.toUpperCase());
 
 const renderTechs = (arr) => {
   techContainer.innerHTML = '';
@@ -16,24 +19,25 @@ const renderTechs = (arr) => {
 };
 renderTechs(techs);
 
-
 const handleSearch = (e) => {
   const { value } = e.target;
-  search = value;
+  search = value.toUpperCase();
   searching();
 };
 inputSearch.addEventListener('change', handleSearch);
 
 const searching = () => {
-  if (techs.includes(search)) {
-    const searchResult = techs.filter(item => item === search);
-    renderTechs(searchResult);
-  }
-  else if (inputSearch.value === '') {
+  if (inputSearch.value === '') {
     renderTechs(techs);
-  }
-  else {
-    techContainer.innerHTML = `Tecnologia '${inputSearch.value}' não encontrada!`;
-  }
+  };
+  let techsIncludesSearchs = [];
+  for(let i = 0; i < techs.length; i++) {
+    if(techs[i].includes(search)) {
+      techsIncludesSearchs.push(techs[i]);
+      renderTechs(techsIncludesSearchs);
+    }
+  };
+  const notFound = () => techContainer.innerHTML = `Tecnologia '${inputSearch.value}' não encontrada!`
+  techsIncludesSearchs.length === 0 && notFound();
 };
 
